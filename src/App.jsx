27a7b8245c5fae7a2ce9e6f72081c71de1258af9
tsubasa <kd1342160@st.kobedenshi.ac.jp>
2025-06-12 11:40:22 +0900

    Appコンポーネントの認証機能を追加し、CSSファイルを削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/firebase';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { signOut } from "firebase/auth";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [isLoginPage, setIsLoginPage] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log('ログイン状態:', currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+  const handleLogout = async () => {
+    try {
+      await signOut(auth); // FirebaseのsignOutでログアウト
+      console.log('ログアウト成功');
+    } catch (err) {
+      console.error('ログアウト失敗：', err.message);
+    }
+  };
+
+  if (user) {
+    return (
+      <div className="container d-flex justify-content-center align-items-center bg-light" style={{ minHeight: '100vh' }}>
+        <div className="card shadow-sm border-0 p-4 rounded-4" style={{ width: '100%', maxWidth: '400px' }}>
+          <h2 className="text-center mb-4 fw-bold text-success">ようこそ、{user.email}さん！</h2>
+          <p className="text-center">ログイン状態です。</p>
+
+          <button className="btn btn-outline-danger" onClick={handleLogout}>
+            ログアウト
+
+          </button>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {isLoginPage ? (
+        <Login setIsLoginPage={setIsLoginPage} />
+      ) : (
+        <Register setIsLoginPage={setIsLoginPage} />
+      )}
+    </div>
+  );
 }
 
 export default App
